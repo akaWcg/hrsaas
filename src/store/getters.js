c438e5d26ec1ed4a76eafc4586d7408fa602474c
陈洋 <3065553845@qq.com>
2022-12-08 19:46:51 +0800

    公司设置基本完成

--- src/store/getters.js
+++ src/store/getters.js
@@ -4,6 +4,7 @@ const getters = {
   token: state => state.user.token, // token 的快速获取
   name: state => state.user.userInfo.username, // 建立对于用户名的快捷访问
   userId: state => state.user.userInfo.userId,
-  staffPhoto: state => state.user.userInfo.staffPhoto
+  staffPhoto: state => state.user.userInfo.staffPhoto,
+  companyId: state => state.user.userInfo.companyId
 }
 export default getters
--- src/views/setting/index.vue
+++ src/views/setting/index.vue
@@ -1,5 +1,5 @@
 <template>
-  <div class="dashboard-container">
+  <div v-loading="loading" class="dashboard-container">
     <div class="app-container">
       <el-card>
         <el-tabs>
@@ -7,7 +7,11 @@
           <el-tab-pane label="角色管理">
             <!-- 新增角色按钮 -->
             <el-row style="height: 60px">
-              <el-button icon="el-icon-plus" size="small" type="primary"
+              <el-button
+                icon="el-icon-plus"
+                size="small"
+                type="primary"
+                @click="showDialog = true"
                 >新增角色</el-button
               >
             </el-row>
@@ -27,9 +31,21 @@
               />
               <el-table-column align="center" prop="description" label="描述" />
               <el-table-column align="center" label="操作">
-                <el-button size="small" type="success">分配权限</el-button>
-                <el-button size="small" type="primary">编辑</el-button>
-                <el-button size="small" type="danger">删除</el-button>
+                <template slot-scope="{ row }">
+                  <el-button size="small" type="success">分配权限</el-button>
+                  <el-button
+                    size="small"
+                    type="primary"
+                    @click="editRole(row.id)"
+                    >编辑</el-button
+                  >
+                  <el-button
+                    size="small"
+                    type="danger"
+                    @click="deleteRole(row.id)"
+                    >删除</el-button
+                  >
+                </template>
               </el-table-column>
             </el-table>
             <!-- 分页组件 -->
@@ -58,16 +74,29 @@
             />
             <el-form label-width="120px" style="margin-top: 50px">
               <el-form-item label="公司名称">
-                <el-input disabled style="width: 400px" />
+                <el-input
+                  v-model="formData.name"
+                  disabled
+                  style="width: 400px"
+                />
               </el-form-item>
               <el-form-item label="公司地址">
-                <el-input disabled style="width: 400px" />
+                <el-input
+                  v-model="formData.companyAddress"
+                  disabled
+                  style="width: 400px"
+                />
               </el-form-item>
               <el-form-item label="邮箱">
-                <el-input disabled style="width: 400px" />
+                <el-input
+                  v-model="formData.mailbox"
+                  disabled
+                  style="width: 400px"
+                />
               </el-form-item>
               <el-form-item label="备注">
                 <el-input
+                  v-model="formData.remarks"
                   type="textarea"
                   :rows="3"
                   disabled
@@ -79,11 +108,43 @@
         </el-tabs>
       </el-card>
     </div>
+    <!-- 弹层 -->
+    <el-dialog title="编辑弹层" :visible="showDialog" @close="btnCancel">
+      <el-form
+        ref="roleForm"
+        :model="roleForm"
+        :rules="rules"
+        label-width="120px"
+      >
+        <el-form-item label="角色名称" prop="name">
+          <el-input v-model="roleForm.name"></el-input>
+        </el-form-item>
+        <el-form-item label="角色描述">
+          <el-input v-model="roleForm.description"></el-input>
+        </el-form-item>
+        <el-row type="flex" justify="center">
+          <el-col :span="6">
+            <el-button size="small" type="primary" @click="btnOK"
+              >确认</el-button
+            >
+            <el-button size="small" @click="btnCancel">取消</el-button>
+          </el-col>
+        </el-row>
+      </el-form>
+    </el-dialog>
   </div>
 </template>
 
 <script>
-import { getRoleList } from '@/api/setting'
+import {
+  getRoleList,
+  getCompanyInfo,
+  deleteRole,
+  getRoleDetail,
+  updateRole,
+  addRole
+} from '@/api/setting'
+import { mapGetters } from 'vuex'
 export default {
   data() {
     return {
@@ -93,21 +154,79 @@ export default {
         page: 1,
         pagesize: 10,
         total: 0 // 记录总数
+      },
+      formData: {},
+      loading: false,
+      showDialog: false,
+      roleForm: {},
+      rules: {
+        name: [{ required: true, message: '角色名称不能为空', trigger: 'blur' }]
       }
     }
   },
   created() {
     this.getRoleList()
+    this.getCompanyInfo()
+  },
+  computeds: {
+    ...mapGetters(['companyId'])
   },
   methods: {
     async getRoleList() {
+      this.loading = true
       const { total, rows } = await getRoleList(this.page)
       this.page.total = total
       this.list = rows
+      this.loading = false
     },
     changePage(newpage) {
       this.page.page = newpage
       this.getRoleList()
+    },
+    async getCompanyInfo() {
+      this.formData = await getCompanyInfo(this.companyId)
+    },
+    async deleteRole(id) {
+      try {
+        await this.$confirm('确认删除该角色吗')
+        await deleteRole(id)
+        this.getRoleList()
+        this.$message.success('删除角色成功')
+      } catch (err) {
+        console.log(err)
+      }
+    },
+    async editRole(id) {
+      try {
+        this.roleForm = await getRoleDetail(id)
+      } catch (err) {
+        console.log(err)
+      }
+      this.showDialog = true
+    },
+    btnCancel() {
+      this.roleForm = {
+        name: '',
+        description: ''
+      }
+      this.$refs.roleForm.resetFields()
+      this.showDialog = false
+    },
+    async btnOK() {
+      try {
+        await this.$refs.roleForm.validate()
+        if (this.roleForm.id) {
+          await updateRole(this.roleForm)
+          this.$message.success('修改成功')
+        } else {
+          await addRole(this.roleForm)
+          this.$message.success('添加成功')
+        }
+        this.getRoleList()
+        this.showDialog = false
+      } catch (err) {
+        console.log(err)
+      }
     }
   }
 }
